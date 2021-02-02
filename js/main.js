8ce7f2f524c6d113f9081538b8989dc63eef95eb
ministrov <61511030+ministrov@users.noreply.github.com>
2021-02-02 15:28:38 +0300

    Ended task with creating functions

--- js/main.js
+++ js/main.js
@@ -3,21 +3,23 @@
 // Получение случайного целого числа в заданном интервале, включительно
 
 const getRandomInteger = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  return Math.floor(Math.random() * (max - min + 1) + min);
+if (min < 0 || max < 0) {
+  return -1;
 }
 
-getRandomInteger(3, 78);
+if (max < min) {
+  [min, max] = [max, min];
+}
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+ let integerUsing = getRandomInteger(3, 78);
 
 // Функция для проверки максимальной длины строки
 
-const getMaxStringLength = function (str) {
-  let maxLength = 10;
-  if (str.length < maxLength) {
-    return maxLength;
-  }
+const getMaxStringLength = function (text, sign) {
+  return text.length <= sign ? true : false;
 }
 
-getMaxStringLength('Hello World');
+let maxLengthUsing = getMaxStringLength('Hello World', 10);
