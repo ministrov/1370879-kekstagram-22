fd59f6c1b5946213b92d47a917cad7e1294067b5
ministrov <61511030+ministrov@users.noreply.github.com>
2021-02-25 21:16:56 +0300

    Finished working on upload input

--- js/upload-picture.js
+++ js/upload-picture.js
@@ -1,7 +1,22 @@
 const editingForm = document.querySelector('.img-upload__overlay');
 const closeEditingForm = editingForm.querySelector('.img-upload__cancel');
-editingForm.classList.remove('hidden');
-document.body.classList.add('modal-open');
-closeEditingForm.addEventListener('click', function () {
+
+const uploadInput = document.querySelector('.img-upload__input');
+
+const onCloseEditingFormClick = function () {
   editingForm.classList.add('hidden');
-})
+  document.body.classList.remove('modal-open');
+
+  uploadInput.value = '';
+
+  closeEditingForm.removeEventListener('click', onCloseEditingFormClick);
+};
+
+uploadInput.addEventListener('change', function (evt) {
+  if (evt.target.value !== '') {
+    editingForm.classList.remove('hidden');
+    document.body.classList.add('modal-open');
+
+    closeEditingForm.addEventListener('click', onCloseEditingFormClick);
+  }
+});
