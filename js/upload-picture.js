87070d52c29c6260bba5fbfe86a3513d074400d7
ministrov <61511030+ministrov@users.noreply.github.com>
2021-03-01 22:55:50 +0300

    Fixing problems with a nouislider

--- js/upload-picture.js
+++ js/upload-picture.js
@@ -1,14 +1,29 @@
+/* noUiSlider */
+
+let scale = 100;
+
+const Size = {
+  MAX: 100,
+  MIN: 25,
+};
+
+const Slider = {
+  MAX: 100,
+  MIN: 0,
+  STEP: 1,
+};
+
 const editingForm = document.querySelector('.img-upload__overlay');
 const closeEditingForm = editingForm.querySelector('.img-upload__cancel');
 const scaleControlSmaller = editingForm.querySelector('.scale__control--smaller');
 const scaleControlBigger = editingForm.querySelector('.scale__control--bigger');
 const scaleControlInput = editingForm.querySelector('.scale__control--value');
 const uploadInput = document.querySelector('.img-upload__input');
-let scale = 100;
 const effectRadioGroup = editingForm.querySelector('.img-upload__effects');
-const effectLevel = editingForm.querySelector('.img-upload__effect-level');
+// const effectLevel = editingForm.querySelector('.img-upload__effect-level');
 const effectLevelSlider = editingForm.querySelector('.effect-level__slider');
 const uploadPreviewImg = editingForm.querySelector('.img-upload__preview').querySelector('img');
+const effectLevelValue = editingForm.querySelector('.effect-level__value');
 
 const onCloseEditingFormClick = function () {
   editingForm.classList.add('hidden');
@@ -30,48 +45,58 @@ uploadInput.addEventListener('change', function (evt) {
 
 
 scaleControlBigger.addEventListener('click', function () {
-  if (scale < 100) {
+  if (scale < Size.MAX) {
     scale += 25;
-    scaleControlInput.value = `${scale}%`;
   }
+  scaleControlInput.value = `${scale}%`;
 });
 
 scaleControlSmaller.addEventListener('click', function () {
   if (scale > 25) {
     scale -= 25;
-    scaleControlInput.value = `${scale}%`;
   }
+  scaleControlInput.value = `${scale}%`;
 });
 
-effectRadioGroup.addEventListener('click', function (evt) {
-  if (evt.target.id === 'effect-none') {
-    effectLevel.classList.add('hidden');
-    uploadPreviewImg.classList.remove('effects__preview--chrome');
-    uploadPreviewImg.classList.remove('effects__preview--sepia');
-    uploadPreviewImg.classList.remove('effects__preview--marvin');
-    uploadPreviewImg.classList.remove('effects__preview--phobos');
-    uploadPreviewImg.classList.remove('effects__preview--heat');
-  } else if (evt.target.id === 'effect-chrome') {
-    uploadPreviewImg.classList.add('effects__preview--chrome');
-  } else if (evt.target.id === 'effect-sepia') {
-    uploadPreviewImg.classList.add('effects__preview--sepia');
-  } else if (evt.target.id === 'effect-marvin') {
-    uploadPreviewImg.classList.add('effects__preview--marvin');
-  } else if (evt.target.id === 'effect-phobos') {
-    uploadPreviewImg.classList.add('effects__preview--phobos');
-  } else if (evt.target.id === 'effect-heat') {
-    uploadPreviewImg.classList.add('effects__preview--heat');
-  } else {
-    effectLevel.classList.remove('hidden');
+
+let lastClass = '';
+
+const effects = {
+  chrome: () => {
+    return `grayscale(${parseInt(effectLevelValue.value, 10) * 0.01})`;
+  },
+  none: () => {
+    return 'none';
+  },
+};
+
+const onEffectRadioGroupClick = (evt) => {
+  if (evt.target.classList.contains('effects__preview')) {
+    if (lastClass !== '') {
+      uploadPreviewImg.classList.remove(lastClass);
+    }
+    let currentClass = evt.target.classList[1];
+    lastClass = currentClass;
+
+    uploadPreviewImg.classList.add(currentClass);
+    uploadPreviewImg.style.filter = effects[currentClass.replace('effects__preview--', '')]();
   }
-});
+};
+
+effectRadioGroup.addEventListener('click', onEffectRadioGroupClick);
 
 window.noUiSlider.create(effectLevelSlider, {
   range: {
-    min: 0,
-    max: 100,
+    min: Slider.MIN,
+    max: Slider.MAX,
   },
-  start: 80,
-  step: 1,
+  start: Slider.MAX,
+  step: Slider.STEP,
   connect: 'lower',
+});
+
+effectLevelSlider.noUiSlider.on('change', () => {
+  effectLevelValue.value = effectLevelSlider.noUiSlider.get();
+
+  uploadPreviewImg.style.filter = effects[lastClass.replace('effects__preview--', '')]();
 });
