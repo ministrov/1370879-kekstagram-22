c6882a9e98fea52cce5bbf74bc8c8fa0ea1c0cd0
ministrov <61511030+ministrov@users.noreply.github.com>
2021-03-18 23:06:36 +0300

    Operated on util and user-picture module

--- js/user-miniature.js
+++ js/user-miniature.js
@@ -1,7 +1,7 @@
 // Отрисовать фотографии других пользователей
 import openBigPicture from './render-big-picture.js';
 import api from './api.js';
-import {getRandomArrayEl, debounce} from './util.js';
+import {debounce, shuffleArray} from './util.js';
 
 const RANDOM_PHOTO_QUANTITY = 10;
 const RERENDER_DELAY = 500;
@@ -10,21 +10,40 @@ const templateFragment = document.querySelector('#picture').content.querySelecto
 const userComments = templateFragment.querySelector('.picture__comments')
 const userLikes = templateFragment.querySelector('.picture__likes');
 const imgFilter = document.querySelector('.img-filters');
-const buttonDefault = document.querySelector('#filter-default');
-const buttonRandom = document.querySelector('#filter-random');
-const buttonDiscussed = document.querySelector('#filter-discussed');
+const filterForm = document.querySelector('.img-filters__form');
 
 const renderUserImage = ({url, comments, likes}) => {
   const userImage = templateFragment.cloneNode(true);
-  userImage.querySelector('.picture__img').src = url;
+
+  userImage.querySelector('.picture__img').src = `./${url}`;
   userComments.textContent = comments.length;
   userLikes.textContent = likes;
+
   userImage.addEventListener('click', () => {
     openBigPicture({url, comments, likes});
   });
+
   return userImage;
 }
 
+const renderPhotos = (data) => {
+  let fragment = document.createDocumentFragment();
+  data.forEach((item) => {
+    fragment.appendChild(renderUserImage(item));
+  });
+
+  userPictures.appendChild(fragment);
+}
+
+const removePhotos = () => {
+  const pictures = userPictures.querySelectorAll('.picture');
+  if (pictures) {
+    pictures.forEach((photo) => {
+      photo.remove()
+    })
+  }
+}
+/*
 const defaultFilter = (data) => {
   document.querySelectorAll('.picture').forEach((item) => item.remove());
   const fragment = document.createDocumentFragment();
@@ -41,32 +60,38 @@ const discussedFilter = (data) => {
   filtered.forEach((item) => fragment.appendChild(renderUserImage(item)));
   userPictures.appendChild(fragment);
 }
+*/
+let photos = [];
 
-const randomFilter = (data) => {
-  let randomEls = [];
-  const fragment = document.createDocumentFragment();
-  document.querySelectorAll('.picture').forEach((item) => item.remove());
-  while (randomEls.length < RANDOM_PHOTO_QUANTITY) {
-    const randomEl = getRandomArrayEl(data, 0, data.length);
-    if (!randomEls.includes(randomEl)) {
-      randomEls.push(randomEl);
-      fragment.appendChild(renderUserImage(randomEl));
-    }
-  }
-  userPictures.appendChild(fragment);
+const filters = {
+  'filter-default': () => {
+    return photos.slice()
+  },
+  'filter-random': () => {
+    return shuffleArray(photos.slice()).slice(0, RANDOM_PHOTO_QUANTITY)
+  },
+  'filter-discussed': () => {
+    return photos.slice().sort((a, b) => a.comments.length - b.comments.length).reverse();
+  },
 }
 
+const onFilterFormClick = debounce((evt) => {
+  if (evt.target.classList.contains('img-filters__button')) {
+    // убрать подсветку старой кнопки и поставить на новую
+    removePhotos()
+
+    renderPhotos(filters[evt.target.id]())
+  }
+}, RERENDER_DELAY);
+
 const renderUserImages = () => {
-  const fragment = document.createDocumentFragment();
   api.getData().then((data) => {
-    data.forEach((item) => {
-      fragment.appendChild(renderUserImage(item));
-    });
-    userPictures.appendChild(fragment);
+    photos = data.slice();
+
+    renderPhotos(photos)
+
     imgFilter.classList.remove('img-filters--inactive');
-    buttonRandom.addEventListener('click', debounce(() => randomFilter(data), RERENDER_DELAY));
-    buttonDiscussed.addEventListener('click', () => discussedFilter(data));
-    buttonDefault.addEventListener('click', () => defaultFilter(data));
+    filterForm.addEventListener('click', onFilterFormClick)
   }).catch(() => {
     const errorFetchTemplate = document.querySelector('#errorFetch').content;
     const errorFetchBlock = errorFetchTemplate.cloneNode(true);
@@ -75,4 +100,5 @@ const renderUserImages = () => {
 }
 
 renderUserImages();
+
 export {templateFragment};
--- js/util.js
+++ js/util.js
@@ -1,7 +1,8 @@
 // Получение случайного целого числа в заданном интервале, включительно
 
 const Keys = {
-  ESC: 'Escape' || 'Esc',
+  ESC: 'Esc',
+  ESCAPE: 'Escape',
 }
 
 const getRandomInteger = (min, max) => {
@@ -14,6 +15,14 @@ const getRandomInteger = (min, max) => {
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const shuffleArray = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
 }
 
 // Функция для проверки максимальной длины строки
@@ -31,7 +40,7 @@ const getRandomArrayEl = (array, min, max) => {
 
 // Нажатие  на кнопу ESC
 const isEscEvent = (evt) => {
-  return evt.key === Keys.ESC;
+  return evt.key === Keys.ESC || evt.key === Keys.ESCAPE;
 }
 
 // Debounce function
@@ -39,7 +48,7 @@ const isEscEvent = (evt) => {
 const debounce = (cb, ms) => {
   let timeout;
   return () => {
-    const fnCall = () => { cb.apply(this) }
+    const fnCall = () => cb.apply(this)
     clearTimeout(timeout);
     timeout = setTimeout(fnCall, ms);
   }
@@ -49,5 +58,6 @@ export {
   getRandomInteger,
   getRandomArrayEl,
   isEscEvent,
-  debounce
+  debounce,
+  shuffleArray
 }
